b1925aed0086c1279837e117fad4d08f6e0c965e
Frane <franespahija8@gmail.com>
2023-05-11 17:01:14 +0200

    lets go

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [location, setLocation] = useState('');
   const [answer, setAnswer] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [progress, setProgress] = useState(0);  // Add a state variable for progress
+  const [progress, setProgress] = useState(0);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -35,8 +35,8 @@ function App() {
     try {
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/process-pdf`, formData);
       setAnswer(response.data.answer);
-      const fileSizeKb = response.data.file_size_kb;  // Get the file size from the response
-      const estimatedTime = (fileSizeKb / 1024) * 90;  // 1 MB is approx. 90 seconds
+      const fileSizeKb = response.data.file_size_kb; // Get the file size from the response
+      const estimatedTime = (fileSizeKb / 1024) * 90; // 1 MB is approx. 90 seconds
       // Set an interval to update the progress bar
       const interval = setInterval(() => {
         setProgress((oldProgress) => {
@@ -120,15 +120,18 @@ function App() {
             cursor: 'pointer',
           }}
         />
-       </form>
-      {isLoading && (
+      </form>
+      {isLoading && !file && (
         <div style={{ textAlign: 'center', marginTop: '10px', color: '#999' }}>
-          <p>
-            {file
-              ? 'Processing... This may take several minutes depending on the document size.'
-              : 'Processing your request...'}
-          </p>
-          <LinearProgress variant="determinate" value={progress} />  {/* Show progress bar */}
+          <p>Processing your request...</p>
+          <BeatLoader color="#007BFF" />
+        </div>
+      )}
+
+      {isLoading && file && (
+        <div style={{ textAlign: 'center', marginTop: '10px', color: '#999' }}>
+          <p>Processing... This may take several minutes depending on the document size.</p>
+          <LinearProgress variant="determinate" value={progress} />
         </div>
       )}
 
@@ -149,4 +152,3 @@ function App() {
 }
 
 export default App;
-
