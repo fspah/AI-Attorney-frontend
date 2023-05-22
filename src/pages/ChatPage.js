63d9e5069016fbfa373c73f4b02c700e9ac18447
Frane <franespahija8@gmail.com>
2023-05-22 14:09:00 +0200

    answ the next message

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -22,7 +22,7 @@ function ChatPage() {
     // The initial system message
     const systemMessage = {
       role: 'user',
-      content: "You are an expert attorney. If the location isn't provided, ask me for the location/jurisdiction.",
+      content: "You are an expert attorney. Answer the next message. If the location isn't provided, ask me for the location/jurisdiction.",
     };
     setChatHistoryForServer([systemMessage]);
 
